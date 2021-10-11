0b890fa61c1c0164da57302d4cf5bb6121db2f85
ahzongo <abdlhassane.zongo@gmail.com>
2021-10-11 13:21:23 +0200

    numero de port en parametre

--- abdelBella/src/serveur2-abdelBella.c
+++ abdelBella/src/serveur2-abdelBella.c
@@ -9,7 +9,28 @@
 #include <time.h>
 #include <sys/select.h>
 
-int main(){
-    printf("Serveur2 \n");
+int main(int argc , char *argv[]){
+    /*
+    Espace dédié aux déclarations de variables
+    Mettre une brève description de chaque variable des qu'on la declare
+    Donner des noms signifiants aux variables
+    */
+   int port; // port de connexion du serveur
+
+    /*
+
+    Espace dedié au code
+    Essayer d'etre le plus clair possible
+    mettre des commentaires
+    */
+    
+    if(argc >=2){
+        port = atoi(argv[1]);	
+        printf("Connexion port %d \n", port);
+    }else{
+        printf("Pas de port specifie ! \n");
+        return -1;
+    }
+
     return(0);
 }
