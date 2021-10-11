8ca05f2beb1fed444482775d95e81c32067cbdf9
ahzongo <abdlhassane.zongo@gmail.com>
2021-10-11 15:53:12 +0200

    stop and go tcp mechanisme

--- abdelBella/src/serveur1-abdelBella.c
+++ abdelBella/src/serveur1-abdelBella.c
@@ -24,10 +24,11 @@ int main(int argc , char *argv[]){
     int utils_client_port = 2000;
     char client_port[4];
     char ack[RCVSIZE] = "ACK";
-    char synack[RCVSIZE] = "SYNACK";
+    char synack[RCVSIZE] = "SYN-ACK";
     int msg_size;
     int len = sizeof(client_addr);
     int bind_server;
+    int bind_datasoc;
 
     
 
@@ -75,16 +76,140 @@ int main(int argc , char *argv[]){
     
     while (1)
     {
+        //synack[RCVSIZE] = "SYN-ACK";
+        //memcpy(synack, "SYN-ACK",);
         msg_size = recvfrom(server_sfd, (char *)buffer, RCVSIZE, MSG_WAITALL, (struct sockaddr *) &client_addr, &len);
         buffer[msg_size] = '\0';
         printf("Client : %s \n", buffer);
         sprintf(client_port,"%d",utils_client_port);
         strcat(synack, client_port);
+        
+        struct sockaddr_in client_udp;
+        int utils_socket;
+        char client_buffer[RCVSIZE];
+        int client_msg_size;
+        int len = sizeof(client_udp);
+        char ack[RCVSIZE];
+        char seq[6];
+        utils_socket = socket(AF_INET, SOCK_DGRAM, 0);
+        if(utils_socket <0){
+            perror("Socket creation failed \n");
+            exit(EXIT_FAILURE);
+        }
+        memset(&client_udp, 0, sizeof(client_udp));
+
+        client_udp.sin_family = AF_INET;
+        client_udp.sin_port = htons(utils_client_port);
+        client_udp.sin_addr.s_addr = htonl(INADDR_ANY);
+
+        bind_datasoc= bind(utils_socket, ( const struct sockaddr *) &client_udp, sizeof(client_udp));
+        if(bind_datasoc == -1){
+            perror("Error when binding 1 \n");
+            return -1;
+        }
+        //printf("Retour socket %d\n", b1);
+        
+        //msg_size = recvfrom(server_sfd, (char *)buffer, RCVSIZE, MSG_WAITALL, (struct sockaddr *) &client_addr, &len);
+        
+
         printf("Server1 : %s\n", synack);
         sendto(server_sfd,(char *) synack,strlen(synack),MSG_CONFIRM,(struct sockaddr *) &client_addr,len);
         msg_size = recvfrom(server_sfd, (char *)buffer, RCVSIZE, MSG_WAITALL, (struct sockaddr *) &client_addr, &len);
         buffer[msg_size] = '\0';
         printf("Client : %s\n", buffer);
+        
+        int fork_val = fork();
+        utils_client_port = utils_client_port +1;
+        //int fork_val = 0;
+
+        if(fork_val == 0){
+            close(server_sfd);
+            FILE *fp;
+            fd_set set;
+            struct timeval t;
+            int wnd = 10;
+            int read;
+            
+            char non_ack[RCVSIZE];
+
+            FD_ZERO(&set);
+            int s;
+            int sent;
+
+
+            while (1)
+            {
+
+                client_msg_size = recvfrom(utils_socket, (char *)client_buffer, RCVSIZE, MSG_WAITALL, (struct sockaddr *) &client_addr, &len);
+                client_buffer[client_msg_size] = '\0';
+                printf("Sever Received %d \n", client_msg_size);
+                printf("Sever Received %s \n", client_buffer);
+                fp = fopen(client_buffer, "r");
+                if ( fp == NULL ) {
+                    perror( "error" );
+                    exit(-1);
+                }
+                int seq = 1;
+                while(!feof(fp))
+                {
+                    //FD_SET(utils_socket,&set);
+                    t.tv_sec= 5;
+                    t.tv_usec = 0;
+                    //int i = 0;
+                    sprintf(client_buffer, "%06d", seq);
+                    read = fread(buffer,1, RCVSIZE-6,fp);
+                    printf("READ: %d\n", read);
+                    memcpy(client_buffer+6, buffer,read);
+                    sent = sendto(utils_socket,(char *) client_buffer,read+6,MSG_CONFIRM,(struct sockaddr *) &client_addr,len);
+                    printf("Sever send %d \n", sent);
+                    if(sent < 0){
+                        perror("Sent Error");
+                    }     
+                    memset(client_buffer, 0, sizeof(client_buffer));
+                    client_msg_size = recvfrom(utils_socket, client_buffer, read+6, MSG_WAITALL, (struct sockaddr *) &client_addr, &len);
+                    client_buffer[client_msg_size] = '\0';
+                    printf("Client : %d\n", client_msg_size);
+                    memset(client_buffer, 0, sizeof(client_buffer));
+
+                    /*s = select(utils_socket+1, &set, NULL, NULL, &t);
+                    if(FD_ISSET(utils_socket,&set)){
+
+                    }else{
+                        //sent = sendto(utils_socket,(char *) client_buffer,read+6,MSG_CONFIRM,(struct sockaddr *) &client_addr,len);
+                        printf("Temps ecoule sans ack \n");
+                    }*/
+                    //memset(client_buffer, 0, sizeof(client_buffer));
+                    //i = i+1;
+                    seq = seq + 1;
+                    /*while (i< wnd && !feof(fp))
+                    {
+                        
+                    }
+
+                    printf("fenetre \n");*/
+                    
+
+                    /*if(strcmp(client_buffer, ) == 0){
+                        seq = seq + 1;
+
+                    }*/
+
+
+                    
+                }
+                //printf("FIN \n");
+                sent = sendto(server_sfd,(char *) synack,strlen(synack),MSG_CONFIRM,(struct sockaddr *) &client_addr,len);
+                
+
+            }
+
+            exit(0);
+            
+        }else{
+            close(utils_socket);
+        }
+
+
     }
     
 
