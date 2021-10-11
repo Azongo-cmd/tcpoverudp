115e9a1fc30f50e49b17fc81df74bffbe1873f4d
ahzongo <abdlhassane.zongo@gmail.com>
2021-10-11 12:49:18 +0200

    squelette projet

---
+++ abdelBella/src/serveur1-abdelBella.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <time.h>
+#include <sys/select.h>
+
+int main(){
+    printf("Serveur1 \n");
+    return(0);
+}
